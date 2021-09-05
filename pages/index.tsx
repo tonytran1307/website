8649138be1c33e5b56e9762624082c959f6726bb
tonytran1307 <tonytranb2b@gmail.com>
2021-09-05 23:24:30 +0200

    update dcm

--- pages/index.tsx
+++ pages/index.tsx
@@ -63,10 +63,11 @@ export default function Home(): ReactElement {
           </ul>
 
           {/* CTA Description */}
-          <p>
-            Polygon Loot is randomized adventurer gear generated and stored on chain.
-            <br/>Stats, images, and other functionality are intentionally omitted for others to interpret.<br/>
-            If you were not lucky enough with Loot on Ethereum, let's feel free to use Polygon Loot in anyway you want.
+         <p>
+            Loot is randomized adventurer gear generated and stored on chain.
+            <br /> Stats, images, and other functionality are intentionally
+            omitted for others to interpret. <br /> Feel free to use Loot in any
+            way you want.
           </p>
         </div>
 
